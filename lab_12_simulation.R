a8a2af105ea45840ec32324dd524653b4d49a2de
selwyni <jackyl@andrew.cmu.edu>
2018-04-18 11:19:48 -0400

    bugfix on model_select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -10,11 +10,11 @@ model_select <- function(covariates, responses, cutoff) {
   reg1 <- lm(responses ~ covariates)
   sum1 <- summary(reg1)
   pvals <- sum1$coefficients[-1,4]
-  if (sum(pvals) == 0) {
+  if (sum(pvals <= cutoff) == 0) {
     return(vector())
   }
   reg2 <- lm(responses ~ covariates[,(pvals <= cutoff)])
   sum2 <- summary(reg2)
-  return(sum2$coefficients[,4])
+  return(sum2$coefficients[-1,4])
 }
 
