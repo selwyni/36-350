d3424b2db8dd10d3f5e42bb8df6178b164bc3c55
selwyni <jackyl@andrew.cmu.edu>
2018-04-18 11:11:30 -0400

    Added model select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -5,3 +5,16 @@ generate_data <- function(n, p) {
   return(list(covariates = cov,
               responses = vec))
 }
+
+model_select <- function(covariates, responses, cutoff) {
+  reg1 <- lm(responses ~ covariates)
+  sum1 <- summary(reg1)
+  pvals <- sum1$coefficients[-1,4]
+  if (sum(pvals) == 0) {
+    return(vector())
+  }
+  reg2 <- lm(responses ~ covariates[,(pvals <= cutoff)])
+  sum2 <- summary(reg2)
+  return(sum2$coefficients[,4])
+}
+
