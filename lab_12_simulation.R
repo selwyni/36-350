c83d0c1e98db030638947a0bf413ca0c91ad2179
selwyni <jackyl@andrew.cmu.edu>
2018-04-18 11:29:54 -0400

    Added run_simulations

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -18,3 +18,18 @@ model_select <- function(covariates, responses, cutoff) {
   return(sum2$coefficients[-1,4])
 }
 
+run_simulations <- function(n_trials, n, p, cutoff) {
+  pvals <- numeric(0)
+  for (trials in 1:n_trials) {
+    data <- generate_data(n,p)
+    pvals <- c(pvals, model_select(data$covariates, data$responses, cutoff))
+  }
+  
+  hist(x = pvals, main = paste("Histogram of PVals at ", n_trials, " trials, n = ", n, " p = ", p, sep = ''))
+}
+
+for (n in c(100, 1000, 10000)) {
+  for (p in c(10, 20, 50)) {
+    run_simulations(1000, n, p, 0.05)
+  }
+}
